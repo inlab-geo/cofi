147eabac0ecfe8a092f6bbd15f2709f9b657e3b1
Jiawen He <hanghur@gmail.com>
2021-11-02 14:40:26 +1100

    Added BaseInverser

---
+++ CoFI/cofi_inverse/rjmcmc/inverser.py
@@ -0,0 +1,9 @@
+from CoFI.cofi_inverse.base_inverser import BaseInverser
+from lib import rjmcmc
+
+class RJMCMC(BaseInverser):
+    def __init__(self):
+        pass
+
+    def solve(self):
+        pass
