05ff444704beb7e719b5d53f5f78e94f145a42fe
Jiawen He <hanghur@gmail.com>
2021-11-12 13:17:08 +1100

    Progress in fm

--- cofi/cofi_inverse/rjmcmc/tests/test_fm.py
+++ cofi/cofi_inverse/rjmcmc/tests/test_fm.py
@@ -1,4 +1,5 @@
 from os import error
+import sys
 from cofi.cofi_inverse import ReversibleJumpMCMC
 import matplotlib
 import matplotlib.pyplot
@@ -39,7 +40,33 @@ global_parameters = None
 def my_loglikelihood(globalvalues, boundaries, localvalues):
     global x, y, sigma
 
-    # the parameters to this function are:
+    # params:
     # - a list of the global values
     # - a sorted list of the boundary positions including the start and end position
     # - the values in each region (so len(boundaries) == len(localvalues) + 1)
+    # returns:
+    # the error between the proposed model and the data
+
+    phi = 0.0   # error accumulator
+
+    for (xi, yi) in zip(x, y):
+        if (xi < boundaries[0]) or (xi > boundaries[len(boundaries) - 1]):
+            # if the point is outside the range then ignore it (shouldn't happen)
+            continue
+
+        ci = 0
+        while (ci < len(localvalues)) and (xi > boundaries[ci + 1]):
+            ci += 1
+
+        if ci == len(localvalues):
+            print("error: failed to find cell")
+            print(" ", boundaries, xi)
+            sys.exit(-1)
+
+        ym = localvalues[ci][0]
+
+        dy = yi - ym
+        phi = phi + (dy * dy)
+
+    return phi / (2.0 * sigma * sigma)
+        
