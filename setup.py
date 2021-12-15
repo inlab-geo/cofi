dd8b82c630981bd059272fbf6f602362c91b9e48
Jiawen He <hanghur@gmail.com>
2021-12-15 15:17:08 +1100

    Config change

--- setup.py
+++ setup.py
@@ -1,5 +1,12 @@
 # pip install -e .
 
-from setuptools import setup
+import sys
+
+try:
+    from skbuild import setup
+except ImportError:
+    print('Please update pip, you need pip 10 or greater,\n'
+          ' or you need to install the PEP 518 requirements in pyproject.toml yourself', file=sys.stderr)
+    raise
 
 setup()
