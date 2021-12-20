9edc4955c735a0cbd467afd32e08858cce8b5986
Jiawen He <hanghur@gmail.com>
2021-12-20 16:59:35 +1100

    Enabled cython extension

--- cofi/cofi_solvers/linear_regression/_c_solver.pyx
+++ cofi/cofi_solvers/linear_regression/_c_solver.pyx
@@ -1,3 +1,4 @@
+# from ._c_solver import c_solve
 from cofi.cofi_solvers import BaseSolver
 from cofi.cofi_objective import LeastSquareObjective, Model
 
@@ -5,7 +6,7 @@ import numpy as np
 from warnings import warn
 
 
-class SimpleLinearRegression(BaseSolver):
+class SimpleLinearRegressionC(BaseSolver):
     def __init__(self, objective: LeastSquareObjective):
         self.objective = objective
 
@@ -17,8 +18,7 @@ class SimpleLinearRegression(BaseSolver):
 
         G = self.objective.design_matrix()
         Y = self.objective.data_y()
-        # TODO regularisation handling? prior model? (ref: inverseionCourse.curveFitting)
-        # TODO return posterior covariance? (ref: inverseionCourse.curveFitting)
+        # res = c_solve(G, Y)
         res = np.linalg.inv(G.T @ G) @ (G.T @ Y)
         model = Model(
             **dict([("p" + str(index[0]), val) for (index, val) in np.ndenumerate(res)])
