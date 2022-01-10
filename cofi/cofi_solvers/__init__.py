debc73a2a9ddbfe808fa2c78de6316e3b3af55a8
Jiawen He <hanghur@gmail.com>
2022-01-11 10:40:29 +1100

    Progress in Fortran77 (hello world)

--- cofi/cofi_solvers/__init__.py
+++ cofi/cofi_solvers/__init__.py
@@ -9,9 +9,10 @@ or are depended on the implementation done by InLab.
 from .base_solver import BaseSolver
 
 # from .rjmcmc._solver import ReversibleJumpMCMC
+from .linear_regression._python_solver import LRNormalEquation
 from .linear_regression._c_solver import LRNormalEquationC
 from .linear_regression._cpp_solver import LRNormalEquationCpp
-from .linear_regression._python_solver import LRNormalEquation
+from .linear_regression._f77_solver import LRNormalEquationF77
 from .scipy._scipy_optimizer import ScipyOptimizerSolver, ScipyOptimizerLSSolver
 from .petsc._tao import TAOSolver
 
@@ -19,9 +20,10 @@ from .petsc._tao import TAOSolver
 __all__ = [
     "BaseSolver",
     # "ReversibleJumpMCMC",
+    "LRNormalEquation",
     "LRNormalEquationC",
     "LRNormalEquationCpp",
-    "LRNormalEquation",
+    "LRNormalEquationF77",
     "TAOSolver",
     "ScipyOptimizerSolver",
     "ScipyOptimizerLSSolver",
---
+++ cofi/cofi_solvers/linear_regression/lib/_f77_solver_lib.f
@@ -0,0 +1,24 @@
+C FILE: _f77_solver_lib.f
+      subroutine fib(a, n)
+      integer n
+      real*8 a(n)
+      do i=1,n
+          if (i .EQ. 1) then
+              a(i) = 0.0D0
+          elseif (i .EQ. 2) then
+              a(i) = 1.0D0
+          else
+              a(i) = a(i-1) + a(i-2)
+          end if
+      enddo
+      end
+
+      subroutine hello()
+      write ( *, '(a)' ) '  Hello, world!'
+      end
+
+      program main
+      call hello()
+      stop
+      end
+C END FILE _f77_solver_lib.f
