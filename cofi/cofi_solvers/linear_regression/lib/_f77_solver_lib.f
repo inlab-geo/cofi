31126ddcc9de821ecd878511ee5d4e7955a51137
Jiawen He <hanghur@gmail.com>
2022-01-11 11:17:57 +1100

    Progress in Fortran77 (fibonacci)

--- cofi/cofi_solvers/linear_regression/lib/_f77_solver_lib.f
+++ cofi/cofi_solvers/linear_regression/lib/_f77_solver_lib.f
@@ -2,6 +2,9 @@ C FILE: _f77_solver_lib.f
       subroutine fib(a, n)
       integer n
       real*8 a(n)
+Cf2py intent(in) n
+Cf2py intent(out) a
+Cf2py depend(n) a
       do i=1,n
           if (i .EQ. 1) then
               a(i) = 0.0D0
