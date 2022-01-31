4952055bef20e4e142e6cfe6d5287ab0d5f317e3
Jiawen He <hanghur@gmail.com>
2022-01-31 11:58:49 +1100

    Added one test to avoid pytest dry

--- cofi/cofi_objective/tests/test_basefwd.py
+++ cofi/cofi_objective/tests/test_basefwd.py
@@ -1,3 +1,4 @@
+from re import M
 from cofi.cofi_objective import (
     BaseForward,
     LinearFittingFwd,
@@ -15,16 +16,19 @@ model = Model(m1=3, m2=2, m3=5)
 
 
 # ------------------------------ test BaseForward ------------------------------
-x_basefwd = np.linspace(0, 1, 100)
-X_basefwd = np.array([x_basefwd ** o for o in range(3)]).T
-base_fwd = BaseForward(forward=(lambda m, X: X @ m.values()))
-y_basefwd = base_fwd.calc(model, X_basefwd)
+def test_base_foward():
+    x_basefwd = np.linspace(0, 1, 100)
+    X_basefwd = np.array([x_basefwd ** o for o in range(3)]).T
+    base_fwd = BaseForward(forward=(lambda m, X: X @ m.values()))
+    y_basefwd = base_fwd.calc(model, X_basefwd)
+    y_basefwd_true = X_basefwd @ params
+    assert np.array_equal(y_basefwd, y_basefwd_true)
 
-plt.figure(1, figsize=(10, 3))
-a = plt.subplot(1, 3, 1)
-a.plot(x_basefwd, y_basefwd)
-a.set_title("Using BaseForward")
-# plt.show()
+    plt.figure(1, figsize=(10, 3))
+    a = plt.subplot(1, 3, 1)
+    a.plot(x_basefwd, y_basefwd)
+    a.set_title("Using BaseForward")
+    # plt.show()
 
 
 # ------------------------------ test LinearFittingFwd ------------------------------
