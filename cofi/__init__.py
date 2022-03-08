ec4f23dd814380dca9c5c307d3cbb3d0a239d288
Jiawen He <hanghur@gmail.com>
2022-03-08 12:54:55 +1100

    Revert root level module init

--- cofi/__init__.py
+++ cofi/__init__.py
@@ -5,22 +5,9 @@
 __all__ = []
 
 from .model_params import Model
-__all__.append("Model")
-
 from .base_forward import BaseForward, LinearForward, PolynomialForward
-__all__.append("BaseForward")
-__all__.append("LinearForward")
-__all__.append("PolynomialForward")
-
 from .base_objective import BaseObjective, LeastSquareObjective, LinearObjective
-__all__.append("BaseObjective")
-__all__.append("LeastSquareObjective")
-__all__.append("LinearObjective")
-
 from .base_solver import BaseSolver, OptimiserMixin
-__all__.append("BaseSolver")
-__all__.append("OptimiserMixin")
-
 
 from . import cofi_objective
 from . import utils
@@ -31,7 +18,18 @@ from . import samplers
 
 try:
     from . import _version
-
     __version__ = _version.__version__
 except ImportError:
     pass
+
+__all__ = [
+    "BaseSolver",
+    "OptimiserMixin",
+    "Model",
+    "BaseObjective",
+    "LeastSquareObjective",
+    "LinearObjective",
+    "BaseForward",
+    "LinearForward",
+    "PolynomialForward",
+]
