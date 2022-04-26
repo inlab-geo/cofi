50b93f111cbb5253e0f986db5f9c3c6714f31351
Jiawen He <hanghur@gmail.com>
2022-04-26 12:03:24 +1000

    refactor: removed setup.cfg

--- setup.py
+++ setup.py
@@ -25,12 +25,36 @@ with open(str(_ROOT / "src" / "cofi" / "_version.py")) as f:
         raise RuntimeError("unable to read the version from src/cofi/_version.py")
 
 
+# read the contents of the README file
+from pathlib import Path
+this_directory = Path(__file__).parent
+long_description = (this_directory / "README.md").read_text()
+
 setup(
     name="cofi",
     version=VERSION,
-    description="Common Framework for Inference",
     author="InLab",
+    description="Common Framework for Inference",
+    long_description=long_description,
+    long_description_content_type="text/markdown",
+    license="MIT",
+    keywords=["inversion", "inference", "python package", "geoscience", "geophysics"],
+    classifiers=[
+        "Development Status :: 1 - Planning",
+        "Intended Audience :: Education",
+        "Intended Audience :: Science/Research",
+        "Programming Language :: Python",
+        "Programming Language :: Python :: 3",
+        "Programming Language :: Python :: 3.8",
+        "Programming Language :: Python :: 3.9",
+        "Programming Language :: Python :: 3.10",
+        "Programming Language :: Python :: Implementation :: CPython",
+        "Programming Language :: C",
+        "Programming Language :: Fortran",
+        "Topic :: Scientific/Engineering :: Physics",
+    ],
     package_dir={"": "src"},
+    python_requires=">=3.8",
     install_requires=[
         "numpy>=1.20",
         "scipy>=1.0.0",
@@ -38,5 +62,7 @@ setup(
     ],
     extras_require={
         "petsc": ["petsc4py>=3.16.0"],
+        "doc": ["sphinx", "sphinx-book-theme", "sphinx-panels", "nbsphinx"],
+        "test": ["pytest", "matplotlib", "coverage[toml]"],
     },
 )
