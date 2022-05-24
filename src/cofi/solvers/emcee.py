545480c3be1f172cca79506fbdd9dbfc0fed88b6
Jiawen He <hanghur@gmail.com>
2022-05-24 15:34:10 +1000

    feat: progress in EmceeSolver

--- src/cofi/solvers/emcee.py
+++ src/cofi/solvers/emcee.py
@@ -1,29 +1,84 @@
+import inspect
 import numpy as np
-import emcee
+from emcee import EnsembleSampler
 
 from . import BaseSolver
 
 
 class EmceeSolver(BaseSolver):
-    documentation_links = []
-    short_description = ""
-    components_used = []
-    required_in_problem = []
+    documentation_links = [
+        "https://emcee.readthedocs.io/en/stable/user/sampler/#emcee.EnsembleSampler",
+        "https://emcee.readthedocs.io/en/stable/user/sampler/#emcee.EnsembleSampler.sample",
+    ]
+    short_description = (
+        "emcee, an MIT licensed pure-Python implementation of Goodman & Weare’s Affine "
+        "Invariant Markov chain Monte Carlo (MCMC) Ensemble sampler"
+    )
+
+    _emcee_EnsembleSampler_args = dict(inspect.signature(EnsembleSampler).parameters)
+    _emcee_EnsembleSampler_sample_args = dict(inspect.signature(EnsembleSampler.sample).parameters)
+    required_in_problem = ["log_posterior", "initial_model"]
     optional_in_problem = dict()
-    required_in_options = []
-    optional_in_options = dict()
+    required_in_options = ["nwalkers", "ndim", "nsteps"]
+    optional_in_options = {
+        k: v.default
+        for k, v in _emcee_EnsembleSampler_args.items()
+        if k not in {"nwalkers", "ndim", "log_prob_fn", "self"}
+    }
+    optional_in_options.update({
+        k: v.default
+        for k, v in _emcee_EnsembleSampler_sample_args.items()
+        if k not in {"initial_state", "iterations"}
+    })
 
     def __init__(self, inv_problem, inv_options):
         super().__init__(inv_problem, inv_options)
         self.components_used = list(self.required_in_problem)
         self._assign_args()
+        self.sampler = EnsembleSampler(
+            nwalkers=self._nwalkers,
+            ndim=self._ndim,
+            log_prob_fn=self._log_prob_fn,
+            pool=self._pool,
+            moves=self._moves,
+            args=self._args,
+            kwargs=self._kwargs,
+            backend=self._backend,
+            vectorize=self._vectorize,
+            blobs_dtype=self._blobs_dtype,
+            parameter_names=self._parameter_names,
+            a=self._a,
+            postargs=self._postargs,
+            threads=self._threads,
+            live_dangerously=self._live_dangerously,
+            runtime_sortingfn=self._runtime_sortingfn,
+        )
 
     def _assign_args(self):
-        inv_problem = self.inv_problem
-        # assign components in problem to args
         # assign options
         self._assign_args()
+        # assign components in problem to args
+        inv_problem = self.inv_problem
+        self.components_used = list(self.required_in_problem)
+        self._log_prob_fn = inv_problem.log_posterior
+        self._initial_state = inv_problem.initial_model + \
+            1e-4 * np.random.randn(self._nwalkers, self._ndim)
+        # TODO is the above a good choice? (generate starting points for users)
 
     def __call__(self) -> dict:
+        res_state = self.sampler.run_mcmc(
+            initial_state=self._initial_state,
+            nsteps = self._nsteps,
+            log_prob0=self._log_prob0,
+            rstate0=self._rstate0,
+            blobs0=self._blobs0,
+            tune=self._tune,
+            skip_initial_state_check=self._skip_initial_state_check,
+            thin_by=self._thin_by,
+            thin=self._thin,
+            store=self._store,
+            progress=self._progress,
+            progress_kwargs=self._progress_kwargs,
+        )
         return None
 
--- tests/cofi_solvers/test_emcee.py
+++ tests/cofi_solvers/test_emcee.py
@@ -1,3 +1,4 @@
+from emcee import EnsembleSampler
 import numpy as np
 import pytest
 
@@ -5,27 +6,65 @@ from cofi.solvers import EmceeSolver
 from cofi import BaseProblem, InversionOptions
 
 
-def _log_prior(theta):
-    a,b,c,d, log_f = theta
-    if -10.0 < a < 10.0 and -10.0 < b < 10.0 and  -10.0 < c < 10.0 and -10.0 < d < 10.0 and -10.0 < log_f < 1.0:
-        return 0.0
-    return -np.inf
+############### Problem setup #########################################################
+_sample_size = 20
+_x = np.random.choice(np.linspace(-3.5,2.5), size=_sample_size)
+_forward = lambda model: np.vander(_x, N=4, increasing=True) @ model
+_y = _forward(np.array([-6,-5,2,1])) + np.random.normal(0,1,_sample_size)
+_sigma = 1.0                                # common noise standard deviation
+_Cdinv = np.eye(len(_y))/(_sigma**2) # Inverse data covariance matrix
 
-def _log_likelihood(theta, x, y):
-    a,b,c,d, log_f = theta
-    model = a+b*x+c*x**2+d*x**3
-    sigma2 =  np.exp(2* log_f)
-    return -0.5 * np.sum((y - model) ** 2 / sigma2 + np.log(sigma2))
 
-def _log_probability(theta, x, y):
-    lp = _log_prior(theta)
+def log_likelihood(model, data_observed, Cdinv):
+    data_predicted = _forward(model)
+    residual = data_observed - data_predicted
+    _log_likelihood = -0.5 * residual @ (Cdinv @ residual).T
+    return _log_likelihood
+
+def _log_prior_gauss(model, prior_mu, prior_covariance):
+    model_diff = model - prior_mu
+    _log_prior = -0.5 * model_diff @ (prior_covariance @ model_diff).T
+    return _log_prior
+
+def _log_prior_uniform(model, lower_bound, upper_bound):
+    for i in range(len(lower_bound)):
+        if(model[i] < lower_bound[i] or model[i] > upper_bound[i]): # if input model lies outside bounds -> return low probability
+            return -np.inf
+    return 0.0 # model lies within bounds -> return log(1)
+
+def log_prior(model, prior_args):
+    prior_option = prior_args[0]
+    if prior_option == "Gaussian":
+        return _log_prior_gauss(model, *prior_args[1:])
+    elif prior_option == "Uniform":
+        return _log_prior_uniform(model, *prior_args[1:])
+
+def log_posterior(model, data_observed, Cdinv, prior_args):
+    lp = log_prior(model, prior_args)
     if not np.isfinite(lp):
         return -np.inf
-    return lp + _log_likelihood(theta, x, y)
+    return lp + log_likelihood(model, data_observed, Cdinv)
 
+
+############### Begin testing #########################################################
 def test_validate():
     inv_problem = BaseProblem()
-    inv_problem.log_prior = _log_prior
-    inv_problem.log_likelihood = _log_likelihood
-    inv_problem.log_probability = _log_probability
-    
+    _prior_args_gauss = [
+        "Gaussian", 
+        np.diag(np.array([1/5.,1/5.,1/2.,1.])), 
+        np.zeros(4),
+    ]
+    _prior_args_uniform = [
+        "Uniform",
+        np.ones(4) * (-10.),
+        np.ones(4) * 10.,
+    ]
+    _log_prosterior = lambda m: log_posterior(m, _y, _Cdinv, _prior_args_gauss)
+    inv_options = InversionOptions()
+    inv_options.set_tool("emcee")
+    with pytest.raises(ValueError, match=r".*not enough information.*"):
+        emcee_solver = EmceeSolver(inv_problem, inv_options)
+    inv_problem.log_posterior = log_posterior
+    inv_problem.set_initial_model(np.array([0.,0.,0.,0.]))
+    emcee_solver = EmceeSolver(inv_problem, inv_options)
+
