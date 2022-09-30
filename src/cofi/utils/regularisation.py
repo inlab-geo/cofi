fix: regularisation gradient and hessian 2x

src/cofi/utils/regularisation.py
@@ -163,60 +163,68 @@ class QuadraticReg(BaseRegularisation):
     
     - If ``reg_type == "damping"``, then
 
-      - :meth:`reg` produces :math:`\text{reg}=\text{factor}\times||m-m_0||_2^2`
-      - :meth:`gradient` produces
-        :math:`\frac{\partial\text{reg}}{\partial m}=\text{factor}\times(m-m_0)`
-      - :meth:`hessian` produces
-        :math:`\frac{\partial^2\text{reg}}{\partial m}=\text{factor}\times I`
-      - :attr:`matrix` is the identity matrix of size :math:`(M,M)`
-      - where
+      .. toggle::
 
-        - :math:`m_0` is a reference model that you can specify in ``ref_model`` argument 
-          (default to zero)
-        - :math:`M` is the number of model parameters
+        - :meth:`reg` produces :math:`\text{reg}=\text{factor}\times||m-m_0||_2^2`
+        - :meth:`gradient` produces
+          :math:`\frac{\partial\text{reg}}{\partial m}=2\times\text{factor}\times(m-m_0)`
+        - :meth:`hessian` produces
+          :math:`\frac{\partial^2\text{reg}}{\partial m}=2\times\text{factor}\times I`
+        - :attr:`matrix` is the identity matrix of size :math:`(M,M)`
+        - where
+
+          - :math:`m_0` is a reference model that you can specify in ``ref_model`` argument 
+            (default to zero)
+          - :math:`M` is the number of model parameters
 
     - If ``reg_type == "roughening"`` (or equivalently ``"flattening"``),
       then 
+    
+      .. toggle::
 
-      - :meth:`reg` produces :math:`\text{reg}=\text{factor}\times||Dm||_2^2`
-      - :meth:`gradient` produces
-        :math:`\frac{\partial\text{reg}}{\partial m}=\text{factor}\times D^TDm`
-      - :meth:`hessian` produces
-        :math:`\frac{\partial^2\text{reg}}{\partial m}=\text{factor}\times D^TD`
-      - :attr:`matrix` is :math:`D`
-      - where 
-        
-        - :math:`D` matrix helps calculate the first order derivative of :math:`m` and looks like
-          
-          :math:`\begin{pmatrix}-1&1&&&&\\&-1&1&&&\\&&...&...&&\\&&&-1&1&\\&&&&&-1&1\end{pmatrix}`
+        - :meth:`reg` produces :math:`\text{reg}=\text{factor}\times||Dm||_2^2`
+        - :meth:`gradient` produces
+          :math:`\frac{\partial\text{reg}}{\partial m}=2\times\text{factor}\times D^TDm`
+        - :meth:`hessian` produces
+          :math:`\frac{\partial^2\text{reg}}{\partial m}=2\times\text{factor}\times D^TD`
+        - :attr:`matrix` is :math:`D`
+        - where 
+            
+          - :math:`D` matrix helps calculate the first order derivative of :math:`m` and looks like
+            
+            :math:`\begin{pmatrix}-1&1&&&&\\&-1&1&&&\\&&...&...&&\\&&&-1&1&\\&&&&&-1&1\end{pmatrix}`
 
     - If ``reg_type == "smoothing"``, then
 
-      - :meth:`reg` produces :math:`\text{reg}=\text{factor}\times||Dm||_2^2`
-      - :meth:`gradient` produces
-        :math:`\frac{\partial\text{reg}}{\partial m}=\text{factor}\times D^TDm`
-      - :meth:`hessian` produces
-        :math:`\frac{\partial^2\text{reg}}{\partial m}=\text{factor}\times D^TD`
-      - :attr:`matrix` is :math:`D`
-      - where 
-        
-        - :math:`D` matrix helps calculate the second order derivatives of :math:`m` and looks like
-          
-          :math:`\begin{pmatrix}1&-2&1&&&&\\&1&-2&1&&&\\&&...&...&...&&\\&&&1&-2&1&\\&&&&1&-2&1\end{pmatrix}`
+      .. toggle::
+
+        - :meth:`reg` produces :math:`\text{reg}=\text{factor}\times||Dm||_2^2`
+        - :meth:`gradient` produces
+          :math:`\frac{\partial\text{reg}}{\partial m}=2\times\text{factor}\times D^TDm`
+        - :meth:`hessian` produces
+          :math:`\frac{\partial^2\text{reg}}{\partial m}=2\times\text{factor}\times D^TD`
+        - :attr:`matrix` is :math:`D`
+        - where 
+            
+          - :math:`D` matrix helps calculate the second order derivatives of :math:`m` and looks like
+            
+            :math:`\begin{pmatrix}1&-2&1&&&&\\&1&-2&1&&&\\&&...&...&...&&\\&&&1&-2&1&\\&&&&1&-2&1\end{pmatrix}`
 
     - If ``reg_type == None``, then we assume you want to use the argument 
       ``byo_matrix``,
+
+      .. toggle::
       
-      - :meth:`reg` produces :math:`\text{reg}=\text{factor}\times||Dm||_2^2`
-      - :meth:`gradient` produces
-        :math:`\frac{\partial\text{reg}}{\partial m}=\text{factor}\times D^TDm`
-      - :meth:`hessian` produces
-        :math:`\frac{\partial^2\text{reg}}{\partial m}=\text{factor}\times D^TD`
-      - :attr:`matrix` is :math:`D`
-      - where 
-        
-        - :math:`D` matrix is ``byo_matrix`` from the arguments (or identity matrix 
-          if ``byo_matrix is None``)
+        - :meth:`reg` produces :math:`\text{reg}=\text{factor}\times||Dm||_2^2`
+        - :meth:`gradient` produces
+          :math:`\frac{\partial\text{reg}}{\partial m}=2\times\text{factor}\times D^TDm`
+        - :meth:`hessian` produces
+          :math:`\frac{\partial^2\text{reg}}{\partial m}=2\times\text{factor}\times D^TD`
+        - :attr:`matrix` is :math:`D`
+        - where 
+            
+          - :math:`D` matrix is ``byo_matrix`` from the arguments (or identity matrix 
+            if ``byo_matrix is None``)
 
     Parameters
     ----------
@@ -236,7 +244,8 @@ class QuadraticReg(BaseRegularisation):
     Raises
     ------
     ValueError
-        when ...
+        when input arguments don't conform to the standards described above. Check 
+        error message for details.
 
     Examples
     --------
@@ -318,16 +327,16 @@ class QuadraticReg(BaseRegularisation):
         flat_m = self._validate_model(model)
         if self._reg_type == "damping":
             if self._ref_model is None:
-                return self._factor * flat_m
-            return self._factor * (flat_m - self._ref_model)
+                return 2 * self._factor * flat_m
+            return 2 * self._factor * (flat_m - self._ref_model)
         else:
-            return self._factor * self.matrix.T @ self.matrix @ flat_m
+            return 2 * self._factor * self.matrix.T @ self.matrix @ flat_m
     
     def hessian(self, model: np.ndarray) -> np.ndarray:
         if self._reg_type == "damping":
-            return self._factor * np.eye(self._model_size)
+            return 2 * self._factor * np.eye(self._model_size)
         else:
-            return self._factor * self.matrix.T @ self.matrix
+            return 2 * self._factor * self.matrix.T @ self.matrix
 
     @staticmethod
     def _validate_factor(factor):

tests/cofi_utils/test_reg_quadratic.py
@@ -15,25 +15,25 @@ def test_damping():
     reg_val = reg(np.array([1,2,3]))
     assert reg_val == 14
     grad = reg.gradient(np.array([1,2,3]))
-    assert grad[0] == 1
-    assert grad[1] == 2
-    assert grad[2] == 3
+    assert grad[0] == 2
+    assert grad[1] == 4
+    assert grad[2] == 6
     hess = reg.hessian(np.array([1,2,3]))
-    assert hess[0,0] == 1
-    assert hess[1,1] == 1
-    assert hess[2,2] == 1
+    assert hess[0,0] == 2
+    assert hess[1,1] == 2
+    assert hess[2,2] == 2
     # ref model
     reg = QuadraticReg(1, 3, ref_model=np.array([1,1,1]))
     reg_val = reg(np.array([1,2,3]))
     assert reg_val == 5
     grad = reg.gradient(np.array([1,2,3]))
     assert grad[0] == 0
-    assert grad[1] == 1
-    assert grad[2] == 2
+    assert grad[1] == 2
+    assert grad[2] == 4
     hess = reg.hessian(np.array([1,2,3]))
-    assert hess[0,0] == 1
-    assert hess[1,1] == 1
-    assert hess[2,2] == 1
+    assert hess[0,0] == 2
+    assert hess[1,1] == 2
+    assert hess[2,2] == 2
 
 def test_damping_invalid():
     with pytest.raises(ValueError): QuadraticReg(1, 10, "dampingnf")