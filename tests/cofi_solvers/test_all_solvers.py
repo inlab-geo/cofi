93b60c4d7e881baaeb5d121751bb140bc89721a4
Jiawen He <hanghur@gmail.com>
2022-12-13 23:49:59 +1100

    feat: raise warning when options are not in the list

---
+++ tests/cofi_solvers/test_all_solvers.py
@@ -0,0 +1,9 @@
+# ############################### TEST ALL SOLVERS ###############################
+# 1. The solver is added to solvers/__init__.py 
+#        __all__, and solvers_table
+# 2. The four class methods are implemented and they return correct data types
+#        required_in_problem, optional_in_problem
+#        required_in_options, optional_in_options
+# 3. __call__ method returns a dictionary with at least "success" and "model"
+#    as keys
+# ################################################################################
