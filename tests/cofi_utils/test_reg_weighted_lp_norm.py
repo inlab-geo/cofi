44065c5ea1a0202e717e7bc6e5bce044158e68a8
Jiawen He <hanghur@gmail.com>
2023-05-24 14:13:28 +1000

    feat: progress in LpNormRegularization

--- tests/cofi_utils/test_reg_weighted_lp_norm.py
+++ tests/cofi_utils/test_reg_weighted_lp_norm.py
@@ -1,28 +1,52 @@
+import numpy
+
 from cofi.utils import LpNormRegularization
 
 
 def test_p_vals_valid():
-    pass
+    # p = 2
+    reg = LpNormRegularization(model_shape=(2,))
+    assert reg(numpy.array([1,2])) == 5
+    # p = 1
+    # p = 0
+    # p = 0.5
+    # p = inf
 
 def test_p_vals_invalid():
     pass
 
 def test_weighting_damping():
+    reg = LpNormRegularization(model_shape=(2,))
+    assert numpy.array_equal(reg.matrix, numpy.array([[1,0],[0,1]]))
+    # ensure matrix is sparse
     pass
 
 def test_weighting_flattening():
+    # ensure matrix is sparse
     pass
 
 def test_weighting_smoothing():
+    # ensure matrix is sparse
+    pass
+
+def test_weighting_none():
+    # ensure matrix is sparse
     pass
 
 def test_weighting_byo():
+    # ensure matrix is sparse
     pass
 
 def test_weighting_invalid():
     pass
 
 def test_reference_model():
+    pass
+
+def test_model_size_must_be_given():
+    # through model_size
+    # through reference_model
+    # error if neither is supplied
     pass
 
 def test_reference_model_matching_shape():
