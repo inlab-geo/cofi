cc9c8681886fedfa64554efd2fc2e81955b4d7db
auggiemarignier <augustin.marignier@anu.edu.au>
2023-10-26 12:21:21 +1100

    create a test for how the BaseProblem.set_parameterisation works

---
+++ tests/cofi_utils/test_parameterisation.py
@@ -0,0 +1,28 @@
+import numpy as np
+
+from cofi import BaseProblem
+from cofi.utils import BaseParameterisation, Identity
+
+
+rng = np.random.default_rng(42)
+N = 10
+
+
+def test_usage_in_BaseProblem():
+    # The Identity parameterisation multiplies by 1, i.e. it basically does nothing
+    # This test checks that the output for BaseProblem.forward is the same with and
+    # without the Identity parameterisation
+
+    model = rng.random(10)
+    inv_problem = BaseProblem()
+    inv_problem.set_forward(lambda x: x**2)
+
+    # Without parameterisation
+    expected = inv_problem.forward(model)
+
+    # With parameterisation
+    parameterisation = Identity(N)
+    inv_problem.set_parameterisation(parameterisation)
+    actual = inv_problem.forward(model)
+
+    assert np.allclose(expected, actual)
