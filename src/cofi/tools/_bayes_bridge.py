423cb028bb1990d243c4d2cde40e59d6e8a34b60
Jiawen He <hanghur@gmail.com>
2023-11-15 10:26:14 +1100

    style: formatting

--- src/cofi/tools/_bayes_bridge.py
+++ src/cofi/tools/_bayes_bridge.py
@@ -25,12 +25,13 @@ class BayesBridge(BaseInferenceTool):
         return {"perturbation_funcs", "walkers_starting_models"}
 
     @classmethod
-    def optional_in_options(cls) -> dict: 
+    def optional_in_options(cls) -> dict:
         return _inspect_default_options()
 
     @classmethod
-    def available_algorithms(cls) -> set: 
+    def available_algorithms(cls) -> set:
         import bayesbridge as bb
+
         return {s for s in bb.samplers.__all__ if s != "Sampler"}
 
     def __init__(self, inv_problem, inv_options):
@@ -44,22 +45,23 @@ class BayesBridge(BaseInferenceTool):
         res = {
             "success": True,
             "models": samples,
-            "sampler": self._bb_bayes_inversion, 
+            "sampler": self._bb_bayes_inversion,
         }
         return res
-    
+
     @error_handler(
-        when="when initializating bayesbridge.BaseBayesianInversion", 
-        context="in the process of preparing", 
+        when="when initializating bayesbridge.BaseBayesianInversion",
+        context="in the process of preparing",
     )
     def _initialize_bb_inversion(self):
         import bayesbridge as bb
+
         self._bb_bayes_inversion = bb.BaseBayesianInversion(
-            walkers_starting_models=self._params["walkers_starting_models"], 
-            perturbation_funcs=self._params["perturbation_funcs"], 
+            walkers_starting_models=self._params["walkers_starting_models"],
+            perturbation_funcs=self._params["perturbation_funcs"],
             log_posterior_func=self.inv_problem.log_posterior,
-            n_chains=self._params["n_chains"], 
-            n_cpus=self._params["n_cpus"], 
+            n_chains=self._params["n_chains"],
+            n_cpus=self._params["n_cpus"],
         )
 
     @error_handler(
@@ -68,32 +70,35 @@ class BayesBridge(BaseInferenceTool):
     )
     def _call_backend_tool(self):
         self._bb_bayes_inversion.run(
-            sampler=self._params["sampler"], 
-            n_iterations=self._params["n_iterations"], 
-            burnin_iterations=self._params["burnin_iterations"], 
-            save_every=self._params["save_every"], 
-            verbose=self._params["verbose"], 
-            print_every=self._params["print_every"], 
+            sampler=self._params["sampler"],
+            n_iterations=self._params["n_iterations"],
+            burnin_iterations=self._params["burnin_iterations"],
+            save_every=self._params["save_every"],
+            verbose=self._params["verbose"],
+            print_every=self._params["print_every"],
         )
-    
+
 
 @functools.lru_cache(maxsize=None)
 def _inspect_default_options():
     import inspect
     from bayesbridge import BaseBayesianInversion
-    
+
     _bb_inv_init_args = dict(inspect.signature(BaseBayesianInversion).parameters)
     optional_in_options: dict = {
-        k: v.default for k, v in _bb_inv_init_args.items() \
-            if v.default is not inspect._empty
+        k: v.default
+        for k, v in _bb_inv_init_args.items()
+        if v.default is not inspect._empty
     }
     _bb_inv_run_args = dict(inspect.signature(BaseBayesianInversion.run).parameters)
-    optional_in_options.update({
-        k: v.default for k, v in _bb_inv_run_args.items() \
+    optional_in_options.update(
+        {
+            k: v.default
+            for k, v in _bb_inv_run_args.items()
             if v.default is not inspect._empty
-    })
+        }
+    )
     return optional_in_options
-
 
 
 # CoFI -> Ensemble methods -> Bayesian sampling -> Trans-D McMC -> bayesbridge -> Reversible Jump Bayesian Sampling
