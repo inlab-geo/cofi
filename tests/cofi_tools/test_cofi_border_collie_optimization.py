c7b03929237c2ae4d7003b6189943c328cf2e289
Jiawen He <hanghur@gmail.com>
2024-02-15 16:11:26 +1100

    test: fix test seed for border collie alg

--- tests/cofi_tools/test_cofi_border_collie_optimization.py
+++ tests/cofi_tools/test_cofi_border_collie_optimization.py
@@ -1,3 +1,4 @@
+import random
 import numpy as np
 import pytest
 
@@ -25,6 +26,8 @@ def problem_setup():
     return inv_problem, inv_options
 
 def test_run(problem_setup):
+    random.seed(42)
+    np.random.seed(42)
     inv_problem, inv_options = problem_setup
     solver = CoFIBorderCollieOptimization(inv_problem, inv_options)
     res = solver()
@@ -33,11 +36,12 @@ def test_run(problem_setup):
     assert d<0.2
 
 def test_run_initial_model(problem_setup):
+    random.seed(42)
+    np.random.seed(42)
     inv_problem, inv_options = problem_setup
     inv_problem.set_initial_model(-10*np.ones((2,)))
     solver = CoFIBorderCollieOptimization(inv_problem, inv_options)
     res = solver()
     # test if we are somewhat close to the global minimum
-    print(res["model"])
     d=np.sqrt((1-res["model"][0])**2+(1-res["model"][1])**2)
     assert d<0.2
