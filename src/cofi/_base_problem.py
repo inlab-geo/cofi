Fixed processing for sparse matrices, added start of Matern covariance

src/cofi/_base_problem.py
@@ -1635,13 +1635,23 @@ class BaseProblem:
         try:
             res = self.residual(model)
             if self.data_covariance_inv_defined:
-                if _is_diag(self.data_covariance_inv):
-                    weighted_res = np.diag(self.data_covariance_inv) * res
+                Cdinv = self.data_covariance_inv
+                if _is_diag(Cdinv):
+                    if scipy.sparse.issparse(Cdinv):
+                        diag = Cdinv.diagonal()
+                    else:
+                        diag = np.diag(Cdinv)
+                    weighted_res = diag * res
                     return res @ weighted_res
                 else:
-                    return res.T @ self.data_covariance_inv @ res
+                    return res.T @ Cdinv @ res
             elif self.data_covariance_defined and _is_diag(self.data_covariance):
-                weighted_res = res / np.diag(self.data_covariance)
+                Cd = self.data_covariance
+                if scipy.sparse.issparse(Cd):
+                    diag = Cd.diagonal()
+                else:
+                    diag = np.diag(Cd)
+                weighted_res = res / diag
                 return res @ weighted_res
             else:
                 return np.sum(np.square(res))
@@ -1819,11 +1829,17 @@ def _matrix_to_func(_, matrix):
 
 
 def _is_diag(matrix):
-    diag_elem = np.diag(matrix).copy()
-    np.fill_diagonal(matrix, 0)
-    out = (matrix == 0).all()
-    np.fill_diagonal(matrix, diag_elem)
-    return out
+    if scipy.sparse.issparse(matrix):
+        if not isinstance(matrix, scipy.sparse.dia_matrix):
+            matrix = matrix.tocsr()
+        diag = matrix.diagonal()
+        return matrix.nnz == np.count_nonzero(diag)
+    else:
+        diag_elem = np.diag(matrix).copy()
+        np.fill_diagonal(matrix, 0)
+        out = (matrix == 0).all()
+        np.fill_diagonal(matrix, diag_elem)
+        return out
 
 
 # ---------- function wrapper to help make things pickleable --------------------------

src/cofi/tools/_scipy_lstsq.py
@@ -1,5 +1,6 @@
 import functools
 import numpy as np
+import scipy.sparse
 
 from . import BaseInferenceTool, error_handler
 
@@ -99,12 +100,19 @@ class ScipyLstSq(BaseInferenceTool):
                 self._Cd_inv = inv_problem.data_covariance_inv
                 self._components_used.append("data_covariance_inv")
             # check diagonal (for potential shortcut in computation)
-            diag_elem = np.diag(self._Cd_inv).copy()
-            np.fill_diagonal(self._Cd_inv, 0)
-            is_diagonal = (self._Cd_inv == 0).all()
-            np.fill_diagonal(self._Cd_inv, diag_elem)
+            if scipy.sparse.issparse(self._Cd_inv):
+                diag_elem = self._Cd_inv.diagonal()
+                is_diagonal = (self._Cd_inv.nnz == np.count_nonzero(diag_elem))
+            else:
+                diag_elem = np.diag(self._Cd_inv).copy()
+                np.fill_diagonal(self._Cd_inv, 0)
+                is_diagonal = (self._Cd_inv == 0).all()
+                np.fill_diagonal(self._Cd_inv, diag_elem)
             if is_diagonal:
-                _gt_cdinv = self._G.T * diag_elem
+                if scipy.sparse.issparse(self._G):
+                    _gt_cdinv = self._G.T @ scipy.sparse.diags(diag_elem)
+                else:
+                    _gt_cdinv = self._G.T * diag_elem
             else:
                 _gt_cdinv = self._G.T @ self._Cd_inv
             self._a = _gt_cdinv @ self._G